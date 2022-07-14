d654a395b7b3f47e237a59f1823670e65ad76ed4
pasly0920 <inwoo920621@gmail.com>
2022-07-14 10:34:32 +0900

    [Style] Prettier 적용

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,6 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import Hello from "@/component/Hello";
+import Hello from '@/component/Hello';
 
 const Router: React.FC = () => {
   return (
