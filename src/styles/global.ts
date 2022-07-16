56ee6cec7dd6f3c59dac0f90644fd89790d5a6df
pasly0920 <inwoo920621@gmail.com>
2022-07-16 16:06:08 +0900

    [Feat] Font Install using woff2 woff

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -3,6 +3,23 @@ import reset from 'styled-reset';
 
 const GlobalStyle = createGlobalStyle`
     ${reset}
+    html,
+    body,
+    span,
+    div,
+    a,
+    input,
+    textarea,
+    button {
+        font-family: 'NotoSansKR', sans-serif;
+    }
+    a{
+        text-decoration: none;
+    }
+    body{
+        -webkit-font-smoothing: antialiased;
+	    -moz-osx-font-smoothing: grayscale;
+    }
 `;
 
 export default GlobalStyle;
