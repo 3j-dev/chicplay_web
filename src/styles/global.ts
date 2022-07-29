5634898b4063f8775c5652898ad2aafa108ae3d8
pasly0920 <inwoo920621@gmail.com>
2022-07-29 11:22:44 +0900

    [Fix] CP-48 fix reset problem heading text was not expressed properly by reset css

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -19,6 +19,36 @@ const GlobalStyle = createGlobalStyle`
     body{
         -webkit-font-smoothing: antialiased;
 	    -moz-osx-font-smoothing: grayscale;
+        h1{
+            font-size: 2em;
+        }
+        h2{
+            font-size: 1.5em;
+        }
+        h3{
+            font-size: 1.17em;
+        }
+        .texteditor {
+            width: 100%;
+            height: 100%;
+          }
+          
+        .DraftEditor-root {
+            border: 1px solid #eee;
+            margin: 2rem 0;
+            border-radius: 0.5rem;
+            width: 90%;
+            height: 80%;
+        }
+          
+          .DraftEditor-editorContainer {
+            padding: 1.5rem;
+            overflow-y: scroll;
+          }
+          
+          .public-DraftEditor-content {
+            min-height: 100%;
+          }
     }
 `;
 
