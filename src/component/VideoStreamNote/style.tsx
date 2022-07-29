610ca89550c186e9b453d9063b5eb623a69ba832
pasly0920 <inwoo920621@gmail.com>
2022-07-29 11:23:22 +0900

    [Feat] CP-48 필기 기능 플러그인 추가 h1, h2, h3 ....

--- src/component/VideoStreamNote/style.tsx
+++ src/component/VideoStreamNote/style.tsx
@@ -4,8 +4,8 @@ export const EditorContainer = styled.div`
   width: 40%;
   height: 50%;
   display: flex;
-  flex-direction: row;
-  justify-content: center;
+  flex-direction: column;
+  justify-content: flex-start;
   align-items: center;
   border: 2px solid black;
   border-radius: 5px;
