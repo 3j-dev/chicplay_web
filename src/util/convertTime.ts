32f98e2b15f25b9043a7ced2bc81e9c88baa33fc
pasly0920 <inwoo920621@gmail.com>
2022-08-13 21:24:41 +0900

    [Fix] timeConvert delete milisecond

--- src/util/convertTime.ts
+++ src/util/convertTime.ts
@@ -6,26 +6,16 @@ const getSecond = (_number: number, _minute: number): number => {
   return Math.floor(_number - _minute * 60);
 };
 
-const getMilisecond = (_number: number): number => {
-  const tenMultipleNumber = _number * 10;
-  return Math.round(tenMultipleNumber % 10);
-};
-
-const makeString = (_minute: number, _second: number, _milisecond: number): string => {
-  const timeArray: string[] = [
-    String(_minute).padStart(2, '0'),
-    String(_second).padStart(2, '0'),
-    String(_milisecond).padStart(2, '0'),
-  ];
+const makeString = (_minute: number, _second: number): string => {
+  const timeArray: string[] = [String(_minute).padStart(2, '0'), String(_second).padStart(2, '0')];
   return timeArray.join(' : ');
 };
 
 export const convertTime = (_time: number): string => {
   const minute: number = getMinute(_time);
   const second: number = getSecond(_time, minute);
-  const milisecond: number = getMilisecond(_time);
 
-  const timeString: string = makeString(minute, second, milisecond);
+  const timeString: string = makeString(minute, second);
 
   return timeString;
 };
