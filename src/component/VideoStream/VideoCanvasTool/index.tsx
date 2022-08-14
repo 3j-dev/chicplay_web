6edd76377d138b9bce2ba5e7d8f5f5b242a81d8e
pasly0920 <inwoo920621@gmail.com>
2022-08-14 16:10:54 +0900

    [Feat] canvasOnScreen clearDrawing

--- src/component/VideoStream/VideoCanvasTool/index.tsx
+++ src/component/VideoStream/VideoCanvasTool/index.tsx
@@ -5,18 +5,27 @@ import { VideoCanvasToolContainer } from './style';
 
 interface Props {
   videoCanvasRef: React.MutableRefObject<TldrawApp | null>;
+  noteType: number;
 }
 
-const VideoCanvasTool: React.FC<Props> = ({ videoCanvasRef }: Props) => {
+const VideoCanvasTool: React.FC<Props> = ({ videoCanvasRef, noteType }: Props) => {
   const tlDrawApp = videoCanvasRef.current;
+  const clearDrawing = () => {
+    if (!tlDrawApp) return;
+    const tool = tlDrawApp.useStore.getState().appState.activeTool;
+    tlDrawApp.deleteAll();
+    tlDrawApp.selectTool(tool);
+  };
+
   return (
-    <VideoCanvasToolContainer>
+    <VideoCanvasToolContainer noteType={noteType}>
       <Click onClick={() => tlDrawApp?.selectTool('select')} />
       <Pencil onClick={() => tlDrawApp?.selectTool(TDShapeType.Draw)} />
       <Eraser onClick={() => tlDrawApp?.selectTool('erase')} />
       <ArrowUpRight onClick={() => tlDrawApp?.selectTool(TDShapeType.Arrow)} />
       <Rectangle onClick={() => tlDrawApp?.selectTool(TDShapeType.Rectangle)} />
       <Circle onClick={() => tlDrawApp?.selectTool(TDShapeType.Ellipse)} />
+      <Trash onClick={clearDrawing} />
     </VideoCanvasToolContainer>
   );
 };
