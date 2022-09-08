c1a49c924b81c6cf277b17675de87c566873c6de
pasly0920 <inwoo920621@gmail.com>
2022-09-08 14:57:37 +0900

    [fix] froala config seperate

--- src/component/VideoNote/MarkdownNote/index.tsx
+++ src/component/VideoNote/MarkdownNote/index.tsx
@@ -1,11 +1,9 @@
 import FroalaEditor from 'react-froala-wysiwyg';
 import Froala from 'froala-editor';
-import FormData from 'form-data';
-import axios from 'axios';
 import { useState, useEffect, useRef } from 'react';
 
 import { FroalaContainer } from './style';
-import { FROALA_TEXT_BUTTONS, FROALA_RICH_BUTTONS, FROALA_PLUGINS } from '@/util/Constant';
+import { config } from './config';
 
 import 'froala-editor/css/froala_style.min.css';
 import 'froala-editor/css/froala_editor.pkgd.min.css';
@@ -62,57 +60,7 @@ const MarkdownNote: React.FC<MarkdownNoteProps> = ({
         model={model}
         onModelChange={handleModelChange}
         tag="textarea"
-        config={{
-          imageMaxSize: 10 * 1024 * 1024,
-          imageDefaultAlign: 'left',
-          imageDefaultDisplay: 'inline-block',
-          imageAllowedTypes: ['jpeg', 'jpg', 'png'],
-          events: {
-            'image.beforeUpload': (images) => {
-              const formData = new FormData();
-              formData.append('multipartFile', images[0]);
-
-              axios
-                .post(process.env.SNAPSHOT_API as string, formData, {
-                  headers: {
-                    'Content-Type': 'multipart/form-data',
-                  },
-                })
-                .then((res) => {
-                  editorInstance.current?.editor.image.insert(
-                    res.data[0].filePath,
-                    null,
-                    null,
-                    editorInstance.current?.editor.image.get(),
-                  );
-                })
-                .catch((err) => {
-                  console.log(err);
-                });
-              return false;
-            },
-          },
-          attribution: false,
-          placeholder: '<h1>제목</h1><h4>내용을 입력해주세요.</h4>',
-          toolbarButtons: {
-            moreText: {
-              buttons: FROALA_TEXT_BUTTONS,
-              buttonsVisible: 8,
-              align: 'center',
-            },
-            moreRich: {
-              buttons: FROALA_RICH_BUTTONS,
-              buttonsVisible: 7,
-              align: 'center',
-            },
-          },
-          // Change buttons for XS screen.
-          toolbarButtonsXS: [
-            ['undo', 'redo'],
-            ['bold', 'italic', 'underline'],
-          ],
-          pluginsEnabled: FROALA_PLUGINS,
-        }}
+        config={config}
       />
     </FroalaContainer>
   );
