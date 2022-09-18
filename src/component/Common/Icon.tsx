15cd08b95872ebe149cd7404a4908a2edc032e0b
pasly0920 <inwoo920621@gmail.com>
2022-09-18 16:17:35 +0900

    [Refactor] Svg Icon 추상화 초안

---
+++ src/component/Common/Icon.tsx
@@ -0,0 +1,23 @@
+import { ComponentProps } from 'react';
+import { IconBaseProps, IconType } from 'react-icons/lib';
+
+import { Colors } from '@/util/Constant';
+
+interface IconProps extends ComponentProps<'svg'> {
+  isActive: boolean;
+  iconName: string;
+  iconProps?: IconBaseProps;
+}
+
+export const Icon: React.FC<IconProps> = ({ isActive, iconName, iconProps }: IconProps) => {
+  const iconDir = iconName.slice(0, 2).toLowerCase();
+  const getIcon = (): JSX.Element => {
+    const icon: IconType = import(`react-icons/${iconDir}/index.esm.js`).then(
+      (elemList) => elemList[iconName as keyof JSX.Element],
+    );
+    console.log(icon);
+    return icon;
+  };
+  const IconElement: IconType = () => getIcon();
+  return <IconElement color={isActive ? Colors.Blue3 : Colors.Black3} {...iconProps} />;
+};
