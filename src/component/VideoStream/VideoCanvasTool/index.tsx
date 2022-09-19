3c6e267c44f22f27321449f7fa8a976b8bb97b81
pasly0920 <inwoo920621@gmail.com>
2022-09-19 15:38:57 +0900

    [Feat] Draggable VisualIndexing

--- src/component/VideoStream/VideoCanvasTool/index.tsx
+++ src/component/VideoStream/VideoCanvasTool/index.tsx
@@ -1,17 +1,7 @@
-import {
-  BsFillCursorFill,
-  BsFillPenFill,
-  BsFillEraserFill,
-  BsArrowUpRight,
-  BsCircle,
-  BsChevronDown,
-  BsXLg,
-  BsChevronUp,
-  BsTrash,
-} from 'react-icons/bs';
-import { BiRectangle } from 'react-icons/bi';
-import { TldrawApp, TDShapeType } from '@tldraw/tldraw';
-import { useState } from 'react';
+import { BsChevronDown, BsXLg, BsChevronUp } from 'react-icons/bs';
+import { TldrawApp } from '@tldraw/tldraw';
+import rafSchd from 'raf-schd';
+import { useState, useRef } from 'react';
 
 import {
   VideoCanvasToolBar,
@@ -25,6 +15,7 @@ import {
   VideoSnapTime,
 } from './style';
 import CanvasNoteTool from '@/component/VideoNote/CanvasNote/CanvasNoteTool';
+import { timeArr, imageArr } from './temp.data';
 
 interface Props {
   videoCanvasRef: React.MutableRefObject<TldrawApp | null>;
@@ -40,45 +31,35 @@ const VideoCanvasTool: React.FC<Props> = ({
   playerRef,
 }: Props) => {
   const [canvasToolMinimized, setCanvasToolMinimized] = useState<Boolean>(false);
-
-  const tlDrawApp = videoCanvasRef.current;
-  const clearDrawing = () => {
-    if (!tlDrawApp) return;
-    const tool = tlDrawApp.useStore.getState().appState.activeTool;
-    tlDrawApp.deleteAll();
-    tlDrawApp.selectTool(tool);
-  };
-
-  const timeArr: string[] = [
-    '00:00',
-    '00:05',
-    '00:10',
-    '00:15',
-    '00:20',
-    '00:25',
-    '00:30',
-    '00:35',
-    '00:40',
-    '00:45',
-    '00:50',
-  ];
-  const imageArr: string[] = [
-    'https://service-video-storage.s3.ap-northeast-2.amazonaws.com/test_hls/Default/Thumbnails/test_hls.0000000.jpg',
-    'https://service-video-storage.s3.ap-northeast-2.amazonaws.com/test_hls/Default/Thumbnails/test_hls.0000001.jpg',
-    'https://service-video-storage.s3.ap-northeast-2.amazonaws.com/test_hls/Default/Thumbnails/test_hls.0000002.jpg',
-    'https://service-video-storage.s3.ap-northeast-2.amazonaws.com/test_hls/Default/Thumbnails/test_hls.0000003.jpg',
-    'https://service-video-storage.s3.ap-northeast-2.amazonaws.com/test_hls/Default/Thumbnails/test_hls.0000004.jpg',
-    'https://service-video-storage.s3.ap-northeast-2.amazonaws.com/test_hls/Default/Thumbnails/test_hls.0000005.jpg',
-    'https://service-video-storage.s3.ap-northeast-2.amazonaws.com/test_hls/Default/Thumbnails/test_hls.0000006.jpg',
-    'https://service-video-storage.s3.ap-northeast-2.amazonaws.com/test_hls/Default/Thumbnails/test_hls.0000007.jpg',
-    'https://service-video-storage.s3.ap-northeast-2.amazonaws.com/test_hls/Default/Thumbnails/test_hls.0000008.jpg',
-    'https://service-video-storage.s3.ap-northeast-2.amazonaws.com/test_hls/Default/Thumbnails/test_hls.0000009.jpg',
-    'https://service-video-storage.s3.ap-northeast-2.amazonaws.com/test_hls/Default/Thumbnails/test_hls.0000010.jpg',
-  ];
+  const visualIndexingRef = useRef<HTMLDivElement>(null);
+  const [isDrag, setIsDrag] = useState<boolean>(false);
+  const [startX, setStartX] = useState<number>(0);
 
   const snapShotMove = (idx: number): void => {
     if (playerRef && playerRef.current) playerRef.current.currentTime = idx * 5;
   };
+
+  const onDragStart = (event: React.MouseEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    setIsDrag(true);
+    if (visualIndexingRef && visualIndexingRef.current)
+      setStartX(event.pageX + visualIndexingRef.current.scrollLeft);
+  };
+
+  const onDragEnd = () => setIsDrag(false);
+
+  const onDragMove = (event: React.MouseEvent<HTMLDivElement>) => {
+    console.log(event);
+    if (isDrag && visualIndexingRef && visualIndexingRef.current) {
+      const { scrollWidth, clientWidth, scrollLeft } = visualIndexingRef.current;
+
+      visualIndexingRef.current.scrollLeft = startX - event.pageX;
+      if (scrollLeft === 0) setStartX(event.pageX);
+      else if (scrollWidth <= clientWidth + scrollLeft) setStartX(event.pageX + scrollLeft);
+    }
+  };
+
+  const throttledOnDragMove = rafSchd(onDragMove);
 
   return (
     <VideoCanvasToolContainer canvasActivated={canvasActivated}>
@@ -88,13 +69,21 @@ const VideoCanvasTool: React.FC<Props> = ({
           <VideoCanvasText>영상 필기</VideoCanvasText>
         </VideoCanvasTitle>
         <VideoCanvasTools>
-          {videoCanvasRef.current !== null && <CanvasNoteTool tlDrawApp={videoCanvasRef.current} />}
+          {videoCanvasRef.current !== null && (
+            <CanvasNoteTool tlDrawApp={videoCanvasRef.current} isPlusFeatureIn={true} />
+          )}
         </VideoCanvasTools>
         <VideoCanvasMinimize onClick={() => setCanvasToolMinimized((prev) => !prev)}>
           {canvasToolMinimized ? <BsChevronUp color="white" /> : <BsChevronDown color="white" />}
         </VideoCanvasMinimize>
       </VideoCanvasToolBar>
-      <VideoVisualIndexing>
+      <VideoVisualIndexing
+        ref={visualIndexingRef}
+        onMouseDown={onDragStart}
+        onMouseMove={isDrag ? throttledOnDragMove : undefined}
+        onMouseUp={onDragEnd}
+        onMouseLeave={onDragEnd}
+      >
         {timeArr.map((cur, idx) => (
           <VideoSnapImage
             src={imageArr[idx]}
