ce6ab575930b85bcc06673e55c767a22ff7ae30b
pasly0920 <inwoo920621@gmail.com>
2022-10-02 13:59:55 +0900

    [Feat] CP-90 Footer 작업

---
+++ src/component/Footer/style.ts
@@ -0,0 +1,10 @@
+import { Colors } from '@/util/Constant';
+import styled from 'styled-components';
+
+const FooterContainer = styled.footer`
+  width: 100%;
+  height: 100px;
+  background-color: ${Colors.Black3};
+`;
+
+export { FooterContainer };
