f525db43e49400c6c0d8b28c8715ca0f67559508
pasly0920 <inwoo920621@gmail.com>
2022-10-04 10:26:04 +0900

    [Feat] CP-85 Main Feature 추가

---
+++ src/component/Main/Feature/index.tsx
@@ -0,0 +1,49 @@
+import { GiGraduateCap } from 'react-icons/gi';
+
+import {
+  FeatureBigText,
+  FeatureContainer,
+  FeatureContent,
+  FeatureContentGroup,
+  FeatureIcon,
+  FeatureSmallText,
+  FeatureTextSection,
+} from './style';
+import { FeatureData } from './data';
+
+interface FeatureDataProps {
+  bigText: string;
+  smallText: string;
+}
+
+interface FeatureAtomProps extends FeatureDataProps {
+  idx: number;
+}
+
+const FeatureAtom: React.FC<FeatureAtomProps> = ({ idx, bigText, smallText }: FeatureAtomProps) => {
+  return (
+    <FeatureContent>
+      <FeatureIcon idx={idx}>
+        <GiGraduateCap color="white" size={50} />
+      </FeatureIcon>
+      <FeatureTextSection>
+        <FeatureBigText>{bigText}</FeatureBigText>
+        <FeatureSmallText>{smallText}</FeatureSmallText>
+      </FeatureTextSection>
+    </FeatureContent>
+  );
+};
+
+const Feature: React.FC = () => {
+  return (
+    <FeatureContainer>
+      <FeatureContentGroup>
+        {FeatureData.map((cur, idx) => (
+          <FeatureAtom idx={idx} key={idx} bigText={cur.bigText} smallText={cur.smallText} />
+        ))}
+      </FeatureContentGroup>
+    </FeatureContainer>
+  );
+};
+
+export default Feature;
