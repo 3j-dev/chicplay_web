e55160b1f6c37eede78726b987391a569ae2b9d6
pasly0920 <inwoo920621@gmail.com>
2022-10-04 20:04:00 +0900

    [Feat] CP-58 ToVivid 섹션 추가

---
+++ src/component/Main/ToVivid/style.ts
@@ -0,0 +1,36 @@
+import { Colors } from '@/util/Constant';
+import styled from 'styled-components';
+
+const ToVividContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  background-color: ${Colors.Black1};
+`;
+
+const ToVividImage = styled.img`
+  width: 40%;
+  height: 80%;
+`;
+
+const ToVividTextGroup = styled.div`
+    width: 30%;
+    height: 70%
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+
+const ToVividText = styled.span`
+  font-size: 48px;
+  font-weight: 500;
+  line-height: 100px;
+  letter-spacing: -0.07em;
+  color: ${Colors.White};
+`;
+
+export { ToVividContainer, ToVividImage, ToVividTextGroup, ToVividText };
