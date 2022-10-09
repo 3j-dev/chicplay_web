30800a30b5d9184003e02d53fc777f5989645073
pasly0920 <inwoo920621@gmail.com>
2022-10-09 21:50:06 +0900

    [Feat] CP-86 메인 토큰 처리 로직 추가

--- src/page/MainPage/index.tsx
+++ src/page/MainPage/index.tsx
@@ -1,9 +1,24 @@
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import { useEffect } from 'react';
+
 import { Layout } from './style';
 import Main from '@/component/Main';
 import Header from '@/component/Header';
 import Footer from '@/component/Footer';
+import { setAccessToken } from '@/util/auth';
 
 const MainPage: React.FC = () => {
+  const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
+  const token = searchParams.get('token');
+
+  useEffect(() => {
+    if (token) {
+      setAccessToken(token);
+      navigate('');
+    }
+  }, [token, navigate]);
+
   return (
     <Layout>
       <Header />
