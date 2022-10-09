6e91dbd2b7765385baf238bb2900e5cf9c556391
pasly0920 <inwoo920621@gmail.com>
2022-10-09 21:41:14 +0900

    [Feat] Token Refresh route 추가

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -2,9 +2,13 @@ export const apiRoutes = {
   uploadVideo: '/api/videos',
   postImageSnapshot: '/api/videos/{individual-video-id}/snapshot',
   getVideoInfo: '/api/videos/{individual-video-id}',
+
   createVideoSpace: '/api/video-space',
   getVideoSpaceList: '/api/video-space',
   addSpaceAccount: '/api/video-space-participant',
+
   updateTextMemo: '/api/videos/{individual-video-id}/cache/text-memo-states',
   getTextMemo: '/api/{individual-video-id}/cache/text-memo-state-latest',
+
+  refreshToken: '/auth/token',
 };
