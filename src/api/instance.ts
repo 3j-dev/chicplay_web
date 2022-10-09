1434dfd2d2ac98e5d095e039f1939ac29d2305a9
pasly0920 <inwoo920621@gmail.com>
2022-10-09 21:41:42 +0900

    [Feat] Token Refresh 로직 추가

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -1,15 +1,39 @@
-import axios from 'axios';
+import axios, { AxiosError, AxiosResponse } from 'axios';
 
 import { authHeader } from '@/util/auth';
+import { refreshToken } from './user';
+import { setAccessToken } from '@/util/auth';
 
 const baseURL: string = 'https://api.dev.edu-vivid.com';
 
-const instance = axios.create({ baseURL });
-instance.interceptors.request.use((config) => {
-  const newConfig = { ...config };
-  newConfig.withCredentials = true;
-  if (newConfig.headers) newConfig.headers.Authorization = authHeader();
-  return newConfig;
+const instance = axios.create({
+  baseURL: baseURL,
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+    Authorization: authHeader(),
+    withCredentials: true,
+  },
 });
+
+instance.interceptors.response.use(
+  (res: AxiosResponse) => {
+    return res;
+  },
+  async (error: AxiosError) => {
+    try {
+      if (error.response === undefined) {
+        throw Error;
+      }
+      const errResponseStatus = error.response.status;
+
+      if (errResponseStatus === 401) {
+        const accessToken = await refreshToken();
+        setAccessToken(accessToken);
+      }
+    } catch (e) {
+      return Promise.reject(e);
+    }
+  },
+);
 
 export const axiosInstance = instance;
