4fb1eb1c605aa738dba2211f16f4b2faa530830d
pasly0920 <inwoo920621@gmail.com>
2022-10-09 18:39:51 +0900

    [Feat] axios Instance 생성 및 적용

---
+++ src/api/instance.ts
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+import { authHeader } from '@/util/auth';
+
+const baseURL: string = 'https://api.dev.edu-vivid.com';
+
+const instance = axios.create({ baseURL });
+instance.interceptors.request.use((config) => {
+  const newConfig = { ...config };
+  if (newConfig.headers) newConfig.headers.Authorization = authHeader();
+  return newConfig;
+});
+
+export const axiosInstance = instance;
