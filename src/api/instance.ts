5dc037ceca761eb589f3452cd2e88872b12f6c2f
pasly0920 <inwoo920621@gmail.com>
2022-10-09 19:58:56 +0900

    [Feat] axios withCredentials값 추가

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -7,6 +7,7 @@ const baseURL: string = 'https://api.dev.edu-vivid.com';
 const instance = axios.create({ baseURL });
 instance.interceptors.request.use((config) => {
   const newConfig = { ...config };
+  newConfig.withCredentials = true;
   if (newConfig.headers) newConfig.headers.Authorization = authHeader();
   return newConfig;
 });
