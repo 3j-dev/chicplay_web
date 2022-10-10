b20e5fec5ce3c7694e6cc71276707f567943ba39
pasly0920 <inwoo920621@gmail.com>
2022-10-10 22:39:14 +0900

    [Feat] CP-86 LoginModal 추가 관련 Style 및 header 수정

--- src/component/Header/index.tsx
+++ src/component/Header/index.tsx
@@ -1,11 +1,14 @@
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { IoSettingsOutline, IoNotificationsOutline } from 'react-icons/io5';
 import { CgProfile } from 'react-icons/cg';
 
 import { HeaderContainer, HeaderContent, HeaderContentContainer, Logo, UserProfile } from './style';
 import LogoSrc from '@/assets/images/logo_with_text.png';
+import Login from '@/component/Login';
 
 const Header: React.FC = () => {
+  const [loginModalOpen, setIsLoginModalOpen] = useState(false);
   const navigate = useNavigate();
 
   return (
@@ -17,7 +20,8 @@ const Header: React.FC = () => {
           <IoSettingsOutline size={20} color="#333" />
           <UserProfile>
             <h4>홍길동 님</h4>
-            <CgProfile size={26} color="#333" />
+            <CgProfile size={26} color="#333" onClick={() => setIsLoginModalOpen(true)} />
+            <Login isOpen={loginModalOpen} setIsOpen={setIsLoginModalOpen} />
           </UserProfile>
         </HeaderContent>
       </HeaderContentContainer>
