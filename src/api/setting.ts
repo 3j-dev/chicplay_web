98c4db5284126356594d8fdafee0619d1562f68c
pasly0920 <inwoo920621@gmail.com>
2022-10-17 14:01:26 +0900

    [Feat] setting user plus api

---
+++ src/api/setting.ts
@@ -0,0 +1,11 @@
+import { axiosInstance } from './instance';
+import { apiRoutes } from './routes';
+
+export const pluserUserInVideoSpace = async (videoSpaceId: number, userEmail: string) => {
+  const data = await axiosInstance.post(
+    apiRoutes.plusUserInVideoSpace
+      .replace('{video-space-id}', `${videoSpaceId}`)
+      .replace('{user-email}', userEmail),
+  );
+  return data;
+};
