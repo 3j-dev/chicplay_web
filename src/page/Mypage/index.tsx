a4c13082e0ca06b3ca55b6d1de49dbba01a50f7d
pasly0920 <inwoo920621@gmail.com>
2022-10-18 02:10:10 +0900

    [Feat] CP-95 Mypage layout 구성

--- src/page/Mypage/index.tsx
+++ src/page/Mypage/index.tsx
@@ -1,5 +1,63 @@
+import { useLayoutEffect, useState } from 'react';
+
+import { Layout } from './style';
+import Content from '@/component/Common/Content';
+import Submenu from '@/component/Common/Submenu';
+import { dashBoardData, myTempData } from './data';
+import { MyDataT, StudiedVideoT } from '@/interfaces/mypage';
+import { getMyPageDashBoard } from '@/api/mypage';
+import {
+  MyProfile,
+  MyRecentStudy,
+  MyRecentNote,
+  MyStudyStat,
+  MyAuthAccount,
+  MyVideoList,
+} from '@/component/Mypage/MypageAtom';
+
 const Mypage: React.FC = () => {
-  return <div>Mypage</div>;
+  const [myData, setMyData] = useState<MyDataT>(myTempData);
+
+  useLayoutEffect(() => {
+    //get and setMyData
+  }, []);
+
+  return (
+    <Layout>
+      <Submenu
+        allMenuState={dashBoardData}
+        nowMenuState={dashBoardData[0]}
+        isPlusMethodExist={false}
+      />
+      <Content
+        atomInRow={2}
+        atomCount={6}
+        childrens={[
+          <MyProfile email={myData.email} name={myData.name} picture={myData.picture} key={0} />,
+          <MyRecentStudy
+            videoSpaceId={myData.lastStudiedIndividualVideo.videoSpaceId}
+            videoSpaceName={myData.lastStudiedIndividualVideo.videoSpaceName}
+            videoDescription={myData.lastStudiedIndividualVideo.videoDescription}
+            individualVideoId={myData.lastStudiedIndividualVideo.individualVideoId}
+            updatedDate={myData.lastStudiedIndividualVideo.updatedDate}
+            videoTitle={myData.lastStudiedIndividualVideo.videoTitle}
+            videoSpaceDescription={myData.lastStudiedIndividualVideo.videoSpaceDescription}
+            lastAccessTime={myData.lastStudiedIndividualVideo.lastAccessTime}
+            key={1}
+          />,
+          <MyRecentNote key={2} />,
+          <MyStudyStat
+            key={3}
+            videoSpaceCount={myData.videoSpaceCount}
+            totalIndividualVideoCount={myData.totalIndividualVideoCount}
+            completedIndividualVideoCount={myData.completedIndividualVideoCount}
+          />,
+          <MyAuthAccount connectedWebex={myData.connectedWebex} key={4} />,
+          <MyVideoList key={5} dashboardIndividualVideos={myData.dashboardIndividualVideos} />,
+        ]}
+      />
+    </Layout>
+  );
 };
 
 export default Mypage;
