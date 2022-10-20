698b808897831bedf28f0b752834b03a4500be9c
pasly0920 <inwoo920621@gmail.com>
2022-10-20 14:07:21 +0900

    [Feat] Api route 추가

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -6,6 +6,7 @@ export const apiRoutes = {
   createVideoSpace: '/api/video-space',
   getVideoSpaceList: '/api/video-space',
   plusUserInVideoSpace: '/api/video-space/{video-space-id}/{user-email}',
+  getHostVideoSpaceList: '/api/video-space/hosted',
 
   updateTextMemo: '/api/videos/{individual-video-id}/cache/text-memo-states',
   getTextMemo: '/api/{individual-video-id}/cache/text-memo-state-latest',
