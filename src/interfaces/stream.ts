894ec3ea3cacbb8ce8d286612a478299cf3aed5a
pasly0920 <inwoo920621@gmail.com>
2022-10-25 18:02:12 +0900

    [Feat] CP-99 TextMemo Snapshot 관련 Interface 추가

--- src/interfaces/stream.ts
+++ src/interfaces/stream.ts
@@ -5,3 +5,22 @@ export interface IndivudalVideoInfoT {
   visualIndexImageFilePathList: string[];
   createdDate: string;
 }
+
+export interface SnapshotInfoT {
+  filePath: string;
+  time: string;
+}
+
+export interface TextMemoT {
+  individualVideoId: string;
+  stateJson: string;
+  videoTime: VideoTimeT;
+  createdAt: string;
+}
+
+interface VideoTimeT {
+  hour: number;
+  minute: number;
+  second: number;
+  nano: number;
+}
