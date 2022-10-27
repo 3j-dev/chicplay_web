3de9cd26b740eac9bc68df8740bf9fb7521d6bcd
pasly0920 <inwoo920621@gmail.com>
2022-10-27 18:54:07 +0900

    [Fix] API 반환형 통일화

--- src/api/upload.ts
+++ src/api/upload.ts
@@ -1,31 +1,31 @@
+import { WebexRecordingT } from '@/interfaces/upload';
 import FormData from 'form-data';
 
 import { axiosInstance } from './instance';
 import { apiRoutes } from './routes';
 
-export const postWebexLoginCode = async (code: string) => {
+export const postWebexLoginCode = async (code: string) =>
   axiosInstance.post(apiRoutes.postWebexLoginCode.replace('{code}', code));
-};
 
-export const getWebexRecordingList = async () => {
-  const data = axiosInstance.get(apiRoutes.getWebexRecordingList);
-  return data;
-};
+export const getWebexRecordingList = async () =>
+  axiosInstance.get<WebexRecordingT[]>(apiRoutes.getWebexRecordingList);
 
-export const postWebexRecording = async (videoSpaceId: string, recordingId: string) => {
-  axiosInstance.post(
+export const postWebexRecording = async (videoSpaceId: number, recordingId: string) =>
+  axiosInstance.post<{ id: number }>(
     apiRoutes.postWebexRecording
-      .replace('{video-space-id}', videoSpaceId)
+      .replace('{video-space-id}', `${videoSpaceId}`)
       .replace('{recording-id}', recordingId),
   );
-};
 
-export const uploadVideoFile = async (videoSpaceId: string, videoFile: File) => {
+export const uploadVideoFile = async (videoSpaceId: number, videoFile: File) => {
   let videoFormData = new FormData();
-  videoFormData.append('video', videoFile);
-  videoFormData.append('videoInfo', videoFile.name);
-  axiosInstance.post(
-    apiRoutes.uploadVideoFile.replace('{video-space-id}', videoSpaceId),
+  const blob = new Blob([JSON.stringify({ title: videoFile.name, description: videoFile.name })], {
+    type: 'application/json',
+  });
+  videoFormData.append('video', videoFile, { contentType: 'video/mp4' });
+  videoFormData.append('videoInfo', blob, { contentType: 'application/json' });
+  return axiosInstance.post<{ id: number }>(
+    apiRoutes.uploadVideoFile.replace('{video-space-id}', `${videoSpaceId}`),
     videoFormData,
     {
       headers: { 'Content-Type': 'multipart/form-data' },
