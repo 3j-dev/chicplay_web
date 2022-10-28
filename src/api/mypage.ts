4fd0599931399fa8cf087f8fc6688e5218bb5711
pasly0920 <inwoo920621@gmail.com>
2022-10-28 18:46:37 +0900

    [Feat] CP-106 U_05 API 적용

--- src/api/mypage.ts
+++ src/api/mypage.ts
@@ -1,7 +1,5 @@
+import { MyDataT } from '@/interfaces/mypage';
 import { axiosInstance } from './instance';
 import { apiRoutes } from './routes';
 
-export const getMyPageDashBoard = async () => {
-  const data = await axiosInstance.get(apiRoutes.getMyPageDashboard);
-  return data;
-};
+export const getMyPageDashBoard = () => axiosInstance.get<MyDataT>(apiRoutes.getMyPageDashboard);
