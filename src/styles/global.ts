dce307e7ac4e6964a7b1aa226a5b7a24884a294a
pasly0920 <inwoo920621@gmail.com>
2022-11-09 14:52:25 +0900

    [Fix] Export Button 가림 현상 해결

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -90,6 +90,9 @@ const GlobalStyle = createGlobalStyle`
         .fr-toolbar .fr-btn-grp {
             margin: 0 8px 0 10px; !important
         }
+        .fr-second-toolbar{
+            background: transparent; !important
+        }
         .fr-wrapper > .fr-element {
             max-height: 73vh !important;
             overflow-y: auto;
