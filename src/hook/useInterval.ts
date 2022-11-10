1c16016722da67b0c30b1093b01a4cd98258588f
pasly0920 <inwoo920621@gmail.com>
2022-11-10 16:02:58 +0900

    [Fix] useInterval 비정상 작동 해결 time이 아닌 model change시마다 update 되던 오류 해결

--- src/hook/useInterval.ts
+++ src/hook/useInterval.ts
@@ -1,25 +1,27 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef } from 'react';
 
-interface UseIntervalT {
-  (callback: () => void, interval: number): void;
-}
+import useIsomorphicLayoutEffect from './useIsomorphicLayoutEffect';
 
-const useInterval: UseIntervalT = (callback, delay) => {
-  const [savedCallback, setSavedCallback] = useState<() => void>(() => {}); // useState사용
+function useInterval(callback: () => void, delay: number | null) {
+  const savedCallback = useRef(callback);
 
-  useEffect(() => {
-    setSavedCallback(callback);
+  // Remember the latest callback if it changes.
+  useIsomorphicLayoutEffect(() => {
+    savedCallback.current = callback;
   }, [callback]);
 
+  // Set up the interval.
   useEffect(() => {
-    const executeCallback = () => {
-      savedCallback();
-    };
+    // Don't schedule if no delay is specified.
+    // Note: 0 is a valid value for delay.
+    if (!delay && delay !== 0) {
+      return;
+    }
 
-    const timerId = setInterval(executeCallback, delay);
+    const id = setInterval(() => savedCallback.current(), delay);
 
-    return () => clearInterval(timerId);
-  }, [delay, savedCallback]);
-};
+    return () => clearInterval(id);
+  }, [delay]);
+}
 
 export default useInterval;
