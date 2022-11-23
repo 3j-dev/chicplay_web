7ffa689f79f5baf6ace26b3c651369f2073eb7d5
pasly0920 <inwoo920621@gmail.com>
2022-11-23 17:18:35 +0900

    [Feat] CP-116 Footer 문구 추가

--- src/component/Footer/style.ts
+++ src/component/Footer/style.ts
@@ -5,6 +5,10 @@ const FooterContainer = styled.footer`
   width: 100%;
   height: 100px;
   background-color: ${Colors.Black3};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: ${Colors.White1};
 `;
 
 export { FooterContainer };
