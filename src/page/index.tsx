31728f0fa9936791eb8ed755d97f48bb52104a6c
pasly0920 <inwoo920621@gmail.com>
2022-11-28 15:35:44 +0900

    [Delete] Unused Part Delete

--- src/page/index.tsx
+++
@@ -1,7 +0,0 @@
-export { default as Main } from '@/page/MainPage';
-export { default as LectureStream } from '@/page/LectureStreamPage';
-export { default as Redirect } from '@/page/RedirectPage';
-export { default as LectureSetting } from '@/page/LectureSettingPage';
-export { default as LectureSpace } from '@/page/LectureSpacePage';
-export { default as LectureUpload } from '@/page/LectureUploadPage';
-export { default as Mypage } from '@/page/Mypage';
