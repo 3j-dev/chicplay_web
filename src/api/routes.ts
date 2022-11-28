a9dd9b1a0c05fb02ddf5ace6c72dd9e8c0375bb8
pasly0920 <inwoo920621@gmail.com>
2022-11-28 21:02:22 +0900

    [Comment] Api Routes 주석 추가

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -1,9 +1,11 @@
 export const apiRoutes = {
+  // Upload
   uploadVideo: '/api/videos',
   postImageSnapshot:
     '/api/individual-videos/{individual-video-id}/snapshot?video-time={video-time}',
   getVideoInfo: '/api/individual-videos/{individual-video-id}',
 
+  // Setting
   createVideoSpace: '/api/video-space',
   deleteVideoSpace: '/api/video-space/{video-space-id}',
   plusUserInVideoSpace: '/api/video-space/{video-space-id}/{user-email}',
@@ -11,22 +13,26 @@ export const apiRoutes = {
   deleteUserInVideoSpace: '/api/video-space/{video-space-id}/{user-email}',
   deleteVideo: '/api/videos/{video-id}',
 
-  updateTextMemo: '/api/individual-videos/{individual-video-id}/cache/text-memo-state',
-  getTextMemo: '/api/individual-videos/{individual-video-id}/cache/text-memo-state-latest',
-  reflectTextMemoInDB: '/api/individual-videos/{individual-video-id}/text-memo-states',
-
-  refreshToken: '/api/auth/token',
-  logout: '/api/auth/logout',
-  getMyPageDashboard: '/api/my-page/dashboard',
-
-  postWebexLoginCode: '/api/webex/token/{code}',
-  getWebexRecordingList: '/api/webex/recordings',
-  postWebexRecording: '/api/webex/recordings/{video-space-id}/{recording-id}',
-  uploadVideoFile: '/api/videos/{video-space-id}',
-
+  //Stream - VideoSpace & VideoStream
   getVideoSpaceList: '/api/video-space',
   getIndividualVideo: '/api/individual-videos/{individual-video-id}',
   freshVideoAccessTime: '/api/individual-videos/{individual-video-id}/accessed',
   getNoteList: '/api/individual-videos/{individual-video-id}/text-memo-state-history',
   updateVideoProgressRate: '/api/individual-videos/{individual-video-id}/progress-rate/{percent}',
+
+  // Stream - TextMemo
+  updateTextMemo: '/api/individual-videos/{individual-video-id}/cache/text-memo-state',
+  getTextMemo: '/api/individual-videos/{individual-video-id}/cache/text-memo-state-latest',
+  reflectTextMemoInDB: '/api/individual-videos/{individual-video-id}/text-memo-states',
+
+  // User
+  refreshToken: '/api/auth/token',
+  logout: '/api/auth/logout',
+  getMyPageDashboard: '/api/my-page/dashboard',
+
+  // Upload
+  postWebexLoginCode: '/api/webex/token/{code}',
+  getWebexRecordingList: '/api/webex/recordings',
+  postWebexRecording: '/api/webex/recordings/{video-space-id}/{recording-id}',
+  uploadVideoFile: '/api/videos/{video-space-id}',
 };
